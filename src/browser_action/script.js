b8b7ff99ed9d20fe287b64d441932747f6e9274f
ChrisThoma <cthoma@vt.edu>
2015-02-07 15:12:16 -0500

    Added a second table for old games, version 1.1.2 released.

--- src/browser_action/script.js
+++ src/browser_action/script.js
@@ -9,7 +9,25 @@ $(document).ready(function () {
             tr.append("<td>" + json[i].home + "</td>");
             tr.append("<td>" + json[i].gametime + "</td>");
             tr.append("<td>" + json[i].channel + "</td>");
-            $('#schedule').append(tr);
+            yesterday = new Date;
+            yesterday.setDate(yesterday.getDate() - 1)
+            if (new Date(json[i].date) < yesterday) {
+                $('#oldgames').append(tr);
+            } else {
+                $('#schedule').append(tr);
+            }
         }
     });
+
 });
+
+function sortByDate(a, b) {
+    var bkg = chrome.extension.getBackgroundPage();
+    bkg.console.log(a.date);
+    bkg.console.log(new Date(a.date) < new Date.setDate())
+    if (new Date(a.date) > new Date) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
